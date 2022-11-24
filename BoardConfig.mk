3c7c96b42ac4d79e8afc338a414744e9b8dee56a
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-11-24 23:30:29 +0100

    redwood: Drop SDM LiveDisplay service
    * We switched to AOSP colors so messing with color calibration will
      confuse QDCM
    
    Change-Id: Ia22c4c06e922d1b7f9c60b257299634333cc3fe4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -77,7 +77,6 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
 
 DEVICE_MANIFEST_FILE := \
     $(DEVICE_PATH)/hidl/manifest_lahaina.xml \
-    $(DEVICE_PATH)/hidl/manifest_lineage.xml \
     $(DEVICE_PATH)/hidl/manifest_xiaomi.xml \
     $(if $(TARGET_NFC_SUPPORTED_SKUS),$(DEVICE_PATH)/hidl/manifest_no_nfc.xml,)
 
--- device.mk
+++ device.mk
@@ -306,10 +306,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Media
 PRODUCT_PACKAGES += \
     libmm-omxcore \
