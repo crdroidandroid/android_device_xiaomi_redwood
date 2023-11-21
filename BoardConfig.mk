1b271edbb9a212cef80e0e9043997d514a3d6cfd
Peter Cai <peter@typeblog.net>
2023-11-21 16:20:18 +0300

    redwood: Panic to recovery, not bootloader

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -110,6 +110,7 @@ BOARD_KERNEL_CMDLINE += pcie_ports=compat
 BOARD_KERNEL_CMDLINE += iptable_raw.raw_before_defrag=1
 BOARD_KERNEL_CMDLINE += ip6table_raw.raw_before_defrag=1
 BOARD_KERNEL_CMDLINE += kpti=off
+BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 
 # Kernel modules
 BOOT_KERNEL_MODULES := \
