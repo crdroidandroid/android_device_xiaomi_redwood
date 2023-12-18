e975e848810aeac1f61f34e50209a793c423d866
TheStrechh <thestrechh@gmail.com>
2023-12-18 07:10:23 +0530

    redwood: Add missing packages for camera

--- device.mk
+++ device.mk
@@ -120,7 +120,10 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     vendor.qti.hardware.camera.device@1.0.vendor \
-    vendor.qti.hardware.camera.postproc@1.0.vendor
+    vendor.qti.hardware.camera.postproc@1.0.vendor \
+    libcamera2ndk_vendor \
+    libion.vendor \
+    libgui_vendor
 
 PRODUCT_PACKAGES += \
     libMegviiFacepp-0.5.2 \
@@ -238,7 +241,11 @@ PRODUCT_PACKAGES += \
     android.hardware.health-service.qti
 
 PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.base@1.0.vendor \
+    libhidltransport \
     libhidltransport.vendor \
+    libhwbinder \
     libhwbinder.vendor
 
 # HotwordEnrollement
