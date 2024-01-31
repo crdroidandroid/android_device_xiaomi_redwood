3f2b74d5a5f858c44b546f121429cc1520907a65
thepriyanshujangid <priyanshujangid@yahoo.com>
2024-01-31 19:27:35 +0530

    redwood: Build soundtrigger 2.3 impl

--- device.mk
+++ device.mk
@@ -46,7 +46,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
-    android.hardware.soundtrigger@2.2-impl
+    android.hardware.soundtrigger@2.3-impl
 
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/audio/,$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_yupik)
