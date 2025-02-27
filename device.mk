3e25c2f8fdafcfe75473cbaf0314c5f2da3d19d4
Michael Bestas <mkbestas@lineageos.org>
2025-02-28 01:21:09 +0200

    redwood: Enable UFFD GC
    Kernel has support for it now.
    
    Change-Id: I3863ee71477f321fe47646571b857fdc39a1902b

--- device.mk
+++ device.mk
@@ -298,7 +298,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
 # Kernel
-PRODUCT_ENABLE_UFFD_GC := false
+PRODUCT_ENABLE_UFFD_GC := true
 PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
 
 # Keymaster
