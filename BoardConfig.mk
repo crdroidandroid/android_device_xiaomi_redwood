0431dcb6881cb1f5d365c9429a8e151250815bfd
Bruno Martins <bgcngm@gmail.com>
2025-05-11 07:27:14 +0530

    redwood: Move camera override format from reserved to select()

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,7 +52,6 @@ TARGET_NO_BOOTLOADER := true
 
 # Camera
 TARGET_CAMERA_SERVICE_EXT_LIB := //$(DEVICE_PATH):libcameraservice_extension.xiaomi_redwood
-TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED := true
 TARGET_CAMERA_PACKAGE_NAME := com.android.camera
 
 # Display
