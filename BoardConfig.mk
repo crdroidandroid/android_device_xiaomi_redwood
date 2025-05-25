3b8fb0083de3052b02ded883cb64cbc451ba5784
thepriyanshujangid <priyanshujangid@yahoo.com>
2025-05-25 21:09:33 +0530

    redwood: Move camera package name to select()

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,7 +52,6 @@ TARGET_NO_BOOTLOADER := true
 
 # Camera
 TARGET_CAMERA_SERVICE_EXT_LIB := //$(DEVICE_PATH):libcameraservice_extension.xiaomi_redwood
-TARGET_CAMERA_PACKAGE_NAME := com.android.camera
 
 # Display
 TARGET_SCREEN_DENSITY := 440
--- device.mk
+++ device.mk
@@ -146,6 +146,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
 $(call soong_config_set,camera,override_format_from_reserved,true)
+$(call soong_config_set,camera,package_name,com.android.camera)
+
 
 # Codec2
 PRODUCT_PACKAGES += \
