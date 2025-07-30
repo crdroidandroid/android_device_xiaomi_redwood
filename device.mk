fc62b5322651748b71f0cd0c31466493d63cce55
kyasu <kyasu.dev@gmail.com>
2025-07-30 23:06:15 +0900

    redwood: Disable `thumbnail_block_model` of libstagefright
    Change-Id: I86f92335af282d3de508150bbbcb3026993ec48c

--- device.mk
+++ device.mk
@@ -347,6 +347,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/yupik/media_codecs_yupik_v1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_yupik_v1.xml \
     $(LOCAL_PATH)/media/yupik/media_profiles_yupik_v1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_yupik_v1.xml
 
+$(call soong_config_set,stagefright,target_disable_thumbnail_block_model,true)
+
 # Mlipay
 PRODUCT_PACKAGES += \
     vendor.xiaomi.hardware.mlipay@1.0.vendor \
