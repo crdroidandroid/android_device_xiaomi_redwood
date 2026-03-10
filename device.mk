91c31f0fb10b046c4f75ff3cee3552df19bcb44b
LuK1337 <priv.luk@gmail.com>
2026-03-10 09:12:28 +0100

    redwood: Remove fwk-detect lib from PRODUCT_PACKAGES
    See: Iac75d83e6bc46c137c328a2365e6701c210bfe7a
    Change-Id: I6b8571e81e59f438e34b76823f47ff024ec33dc6

--- device.mk
+++ device.mk
@@ -376,9 +376,6 @@ PRODUCT_COPY_FILES += \
 
 $(call soong_config_set,power_libperfmgr,mode_extension_lib,//$(LOCAL_PATH):libperfmgr-ext-xiaomi)
 
-# QTI
-PRODUCT_PACKAGES += \
-    libvndfwk_detect_jni.qti.vendor:64 # Needed by CNE app
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
